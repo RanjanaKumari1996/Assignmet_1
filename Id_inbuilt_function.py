fb0b48110534b6adf9402671ed709280c71aa7e6
RanjanaKumari1996 <112890269+RanjanaKumari1996@users.noreply.github.com>
2022-09-08 20:59:35 +0530

    Add files via upload

---
+++ Id_inbuilt_function.py
@@ -0,0 +1,27 @@
+num=10
+print(num)
+
+memory_loc= id(num)
+
+print("Mrmory Location is:", memory_loc)
+
+num=5000
+print(num)
+memory_loc= id(num)
+print("Memory Loction is:",memory_loc)
+
+num=5000
+print(num)
+memory_loc= id(num)
+print("Memory Loction is:",memory_loc)
+
+
+num= -10
+print(num)
+memory_loc= id(num)
+print("Memory Loction is:",memory_loc)
+
+num1= -10
+print(num)
+memory_loc= id(num1)
+print("Memory Loction is:",memory_loc)
