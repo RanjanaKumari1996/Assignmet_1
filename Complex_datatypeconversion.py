2c95e13a3305facfa45c1ba76b9aa8b5095c57d2
RanjanaKumari1996 <112890269+RanjanaKumari1996@users.noreply.github.com>
2022-09-09 21:50:35 +0530

    Add files via upload

---
+++ Complex_datatypeconversion.py
@@ -0,0 +1,9 @@
+comp=25+69j
+#Conversion of complex
+
+#print("Complex converts in Int=", int(comp))
+#print("Complex converts in Float=", float(comp))
+print("Complex converts in Boolean=", bool(comp))
+print("Complex converts in String=", str(comp))
+print("Complex converts in Complex=", complex(comp))
+#print("Complex converts in List=", list(comp))
