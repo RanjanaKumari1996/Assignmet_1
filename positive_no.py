2cfbca88829ddf6e6cc9356c32b2d423471d3950
RanjanaKumari1996 <112890269+RanjanaKumari1996@users.noreply.github.com>
2022-09-09 21:34:20 +0530

    Add files via upload

---
+++ positive_no.py
@@ -0,0 +1,8 @@
+no=int(input("Please enter a number:"))
+
+if no>0:
+    print("You have entered a positive number")
+elif no<0:
+    print("You have entered a negative number")
+else:
+    print("You have  entered 0")
