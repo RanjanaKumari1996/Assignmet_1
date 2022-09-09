8e8ec93f508870ba84ebf7a2c0f7d0198968149a
RanjanaKumari1996 <112890269+RanjanaKumari1996@users.noreply.github.com>
2022-09-09 21:50:07 +0530

    Add files via upload

--- String_datatypeconversion.py
+++ String_datatypeconversion.py
@@ -10,3 +10,14 @@ print("String converts in String=", str(str1))
 print("String converts in List=", list(str1))
 print("String converts in set=", set(str1))
 #print("String converts in dict=", dict(str1))
+
+
+str2=25968
+
+print("String converts in Int=", int(str2))
+print("String converts in Float=", float(str2))
+print("String converts in Boolean=", bool(str2))
+print("String converts in String=", str(str2))
+print("String converts in Complex=", complex(str2))
+print("String converts in List=", list(str2))
+print("String converts in set=", set(str2))
