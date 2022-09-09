1bb3d28f86512a737876b911b0221cb3f326f78a
RanjanaKumari1996 <112890269+RanjanaKumari1996@users.noreply.github.com>
2022-09-09 21:35:28 +0530

    Add files via upload

---
+++ Float_datatypeconversion.py
@@ -0,0 +1,10 @@
+#Convert in FLOAT
+
+no=25.36998
+
+print("Float converts in Int=", int(no))
+print("Float converts in Float=", float(no))
+print("Float converts in Boolean=", bool(no))
+print("Float converts in String=", str(no))
+print("Float converts in Complex=", complex(no))
+print("Float converts in List=", dict(no))
