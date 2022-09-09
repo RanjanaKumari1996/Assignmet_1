bca246378473e2848b5daac22f64e8fee5b750c0
RanjanaKumari1996 <112890269+RanjanaKumari1996@users.noreply.github.com>
2022-09-09 21:34:59 +0530

    Add files via upload

---
+++ Int_datatypeconversion.py
@@ -0,0 +1,9 @@
+a=10
+#Convert in INT
+
+print("Int converts in Int=", int(a))
+print("Int converts in Float=", float(a))
+print("Int converts in Boolean=", bool(a))
+print("Int converts in String=", str(a))
+print("Int converts in Complex=", complex(a))
+#print("Int converts in List=", dict(a))
---
+++ String_datatypeconversion.py
@@ -0,0 +1,12 @@
+#Convert in STRING
+
+str1="Ranjana Singh Rajput"
+
+#print("String converts in Int=", int(str1))
+#print("String converts in Float=", float(str1))
+print("String converts in Boolean=", bool(str1))
+print("String converts in String=", str(str1))
+#print("String converts in Complex=", complex(str1))
+print("String converts in List=", list(str1))
+print("String converts in set=", set(str1))
+#print("String converts in dict=", dict(str1))
