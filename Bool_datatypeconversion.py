ef19ef355803d296683f01e0e410f1f4e2a3ed8d
RanjanaKumari1996 <112890269+RanjanaKumari1996@users.noreply.github.com>
2022-09-09 21:37:32 +0530

    Add files via upload

---
+++ Bool_datatypeconversion.py
@@ -0,0 +1,9 @@
+bool1=True
+#Convert in BOOLEAN
+
+print("Boolean converts in Int=", int(bool1))
+print("Boolean converts in Float=", float(bool1))
+print("Boolean converts in Boolean=", bool(bool1))
+print("Boolean converts in String=", str(bool1))
+print("Boolean converts in Complex=", complex(bool1))
+#print("Int converts in List=", dict(bool1))
