c2148dc7b66b06577f888c039c8831434bf3624e
RanjanaKumari1996 <112890269+RanjanaKumari1996@users.noreply.github.com>
2022-09-19 21:26:04 +0530

    Add files via upload

---
+++ _6_Conditional_statements/ladder_if_statement.py
@@ -0,0 +1,13 @@
+no1=80
+no2=1000
+
+if no1>=562:
+    print("A")
+if no1==80:
+    print("B")
+if no2>no1:
+    print("C")
+if no2==100:
+    print("D")
+else:
+    print("E")
---
+++ _6_Conditional_statements/multiple_conditions.py
@@ -0,0 +1,7 @@
+no1=40
+no2=90
+
+if no1==40 and no1<=no2:
+    print("Worked")
+else:
+    print("not worked")
