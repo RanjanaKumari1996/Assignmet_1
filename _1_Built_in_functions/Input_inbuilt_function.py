52ae542b7d2172c421e806fd384c2fe0ed1d1deb
RanjanaKumari1996 <112890269+RanjanaKumari1996@users.noreply.github.com>
2022-09-19 21:19:16 +0530

    Add files via upload

---
+++ _1_Built_in_functions/Input_inbuilt_function.py
@@ -0,0 +1,9 @@
+no1= input("Please Enter the first number:")
+print("you have entered first number is :", no1)
+
+no2= input("Please Enter the second number:")
+print("you have entered second number is :", no2)
+
+print("Addition of entered numbers  by default is:", no1+no2)
+
+print("Addition of entered numbers after int conversion is:", int(int(no1)+int(no2)))
