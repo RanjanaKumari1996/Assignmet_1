436fe4f4308ea2d68bff1b34f3d812bf2c89ca80
RanjanaKumari1996 <112890269+RanjanaKumari1996@users.noreply.github.com>
2022-09-21 17:42:53 +0530

    Add files via upload

---
+++ _13_Fucntions/_1_function_demo.py
@@ -0,0 +1,10 @@
+def addition(): #defining the function
+
+    no1=int(input("Enter first number:"))
+    no2=int(input("Enter second number:"))
+
+    add= no1 + no2
+    print("Addition:",add)
+
+
+addition()  #calling the function
---
+++ _13_Fucntions/_2_Two_ways_creating_functions.py
@@ -0,0 +1,56 @@
+# #fucntion without parameter and without return statement
+
+# def addition(): #defining the function
+
+#     no1=int(input("Enter first number:"))
+#     no2=int(input("Enter second number:"))
+
+#     add= no1 + no2
+#     print("Addition:",add)
+
+
+# addition()
+
+
+# ##fucntion without parameter and with return statement
+
+# def multiplication(): #defining the function
+
+#     no1=int(input("Enter first number:"))
+#     no2=int(input("Enter second number:"))
+
+#     mul= no1 * no2
+#     return mul
+#     #print("Addition:",add)
+
+
+# result=multiplication()
+
+# print("Multiplication:", result)
+
+
+
+#function with parameter and without return statement
+
+# def multiplication(no1,no2): #defining the function
+
+#     mul= no1 * no2
+#     print("Multiplication:",mul)
+#     mul= no1 * no2
+#     return mul
+#     print("Multiplication:",mul)
+
+# multiplication(500,20)
+
+
+#function with parameter and with return statement
+
+def multiplication(no1,no2): #defining the function
+
+    mul= no1 * no2
+    return mul
+    #print("Multiplication:",mul)
+
+
+result=multiplication(40,20)
+print(result)
