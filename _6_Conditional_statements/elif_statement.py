126477a2d695dc3196c743d255e566dbbe6ad3fe
RanjanaKumari1996 <112890269+RanjanaKumari1996@users.noreply.github.com>
2022-10-04 23:09:12 +0530

    Add files via upload

---
+++ _6_Conditional_statements/elif_statement.py
@@ -0,0 +1,9 @@
+age= int(input("Please enter your age:"))
+
+
+if age>60:
+    print("Your are not eligible to work, it's your retirement period! Enjoy your LIFE")
+elif age>=18:
+    print("Your are eligible to work in our organisation")
+else:
+    print("You are a kid! Please apply after completing your study")
---
+++ _6_Conditional_statements/if_statement.py
@@ -0,0 +1,20 @@
+# no1= input("Please enter the first value:")
+
+# no2= input("Please enter the second value:")
+
+# #if statement
+# if no1<no2:
+#     print("good evening everyone")
+
+#if executes only if condition is true
+
+# data= True
+# if data:
+#     print("Ranjana")
+
+age= int(input("Please enter your age:"))
+
+if age>=18:
+    print("You are eligible for vote")
+else:
+    print("You are not eligible for vote")
