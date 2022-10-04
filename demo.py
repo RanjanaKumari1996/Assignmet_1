8cc8cf8509e3108db77007a0d4d71c35dc3664c0
RanjanaKumari1996 <112890269+RanjanaKumari1996@users.noreply.github.com>
2022-10-04 23:15:25 +0530

    Add files via upload

--- demo.py
+++ demo.py
@@ -1,3 +1,16 @@
-print("hello Ranjana. how are you?")
-num="PYTHON"
-print(num)
+# print("hello Ranjana. how are you?")
+# num="PYTHON"
+# print(num)
+
+# s='foo'
+# t='bar'
+# print('barf' in 2*(s+t))
+# print(chr(ord('b')+1))
+from re import A
+
+
+# x='abcd'
+# for i in range (0):
+#     print(i)
+
+print('ab,cd,ef,cd,gh.cd'.split(','))
---
+++ test.py
@@ -0,0 +1,4 @@
+import _14_packages_and_modules._2_module as md
+
+md.one()
+print(md.name)
